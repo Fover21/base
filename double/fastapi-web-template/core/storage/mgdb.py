8346954cd2c4f3b17061aeaeb25d5efc8631d66a
Wzy <850781645@qq.com>
2022-02-21 14:18:50 +0800

    原生数据库查询测试

--- double/fastapi-web-template/core/storage/mgdb.py
+++ double/fastapi-web-template/core/storage/mgdb.py
@@ -16,12 +16,17 @@ class MyMongo:
 
     @classmethod
     async def create(cls) -> aiomongo.AioMongoClient:
-        _pool = await aiomongo.create_client(config.MONGO_CACHE_URI)
-        # 操作
-        # db = (await _pool)["bigdata"]
-        # collection = db["bigdata"]
-        # c = await collection.find_one({})
-        cls.pool = _pool
+        _pool = None
+        try:
+            _pool = await aiomongo.create_client(config.MONGO_CACHE_URI)
+            # 操作
+            # db = (await _pool)["bigdata"]
+            # collection = db["bigdata"]
+            # c = await collection.find_one({})
+            cls.pool = _pool
+            return _pool
+        except Exception as e:
+            logger.error(f"连接mongodb异常 {e}")
         return _pool
 
 
--- double/fastapi-web-template/core/storage/rdb.py
+++ double/fastapi-web-template/core/storage/rdb.py
@@ -17,17 +17,24 @@ class MyRedis:
 
     @classmethod
     async def create(cls) -> aioredis.Redis:
-        #     https://aioredis.readthedocs.io/en/latest/getting-started/
-        #     redis://[[username]:[password]]@localhost:6379/0
-        #     rediss://[[username]:[password]]@localhost:6379/0
-        #     unix://[[username]:[password]]@/path/to/socket.sock?db=0
-        #     """
-        #     # _pool = await aioredis.from_url("127.0.0.1", port=6379, db=0, password='', decode_responses=True)
-        _pool = await aioredis.from_url(config.REDIS_CACHE_URI, port=config.REDIS_CACHE_PORT,
-                                        password=config.REDIS_CACHE_PASSWORD, db=config.REDIS_CACHE_DB,
-                                        encoding="utf-8",
-                                        decode_responses=True)
-        cls.pool = _pool
+        _pool = None
+        try:
+            #     https://aioredis.readthedocs.io/en/latest/getting-started/
+            #     redis://[[username]:[password]]@localhost:6379/0
+            #     rediss://[[username]:[password]]@localhost:6379/0
+            #     unix://[[username]:[password]]@/path/to/socket.sock?db=0
+            #     """
+            #     # _pool = await aioredis.from_url("127.0.0.1", port=6379, db=0, password='', decode_responses=True)
+            _pool = await aioredis.from_url(config.REDIS_CACHE_URI, port=config.REDIS_CACHE_PORT,
+                                            password=config.REDIS_CACHE_PASSWORD, db=config.REDIS_CACHE_DB,
+                                            encoding="utf-8",
+                                            decode_responses=True)
+            if not _pool.ping():
+                logger.info("连接redis超时")
+            cls.pool = _pool
+            return _pool
+        except Exception as e:
+            logger.error(f"连接redis异常 {e}")
         return _pool
 
 
